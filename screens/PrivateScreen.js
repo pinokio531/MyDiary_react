6942e0dea1ac2d2ee43ef2b20ba43bcac44c68bf
sungminKim <pinokio0330@gmail.com>
2019-08-31 09:57:43 +0900

    UI Complete

---
+++ screens/PrivateScreen.js
@@ -0,0 +1,13 @@
+// PrivateScreen.js
+
+import React from 'react'
+import { Text } from 'react-native';
+import {SafeAreaView} from 'react-navigation';
+
+export default PrivateScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>PrivateScreen</Text>
+        </SafeAreaView>
+    )
+}
